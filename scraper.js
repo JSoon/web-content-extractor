d3683c2e369daf4636e8f938d85963b602514de0
J.Soon <serdeemail@gmail.com>
2025-07-31 15:52:18 +0800

    fix: [44:44:0731/074138.124615:ERROR:content/browser/zygote_host_impl_linux.cc:105] Running as root without --no-sandbox is not supported. See https://crbug.com/638180. TROUBLESHOOTING: https://pptr.dev/troubleshooting

--- scraper.js
+++ scraper.js
@@ -13,7 +13,7 @@ export async function scrapeToMarkdown(url, savePath = null) {
     url = `http://${url}`;
   }
     
-  const browser = await puppeteer.launch({ headless: 'new' });
+  const browser = await puppeteer.launch({ headless: 'new', args: ['--no-sandbox', '--disable-setuid-sandbox'] });
   const page = await browser.newPage();
 
   try {
